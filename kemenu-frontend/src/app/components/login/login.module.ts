387aed1f8a0af502292ab6fd7539e540d286c67b
afdezcl <adrifdez_@uma.es>
2020-05-07 00:30:26 +0200

    Added form on login and localStorage service

--- kemenu-frontend/src/app/components/login/login.module.ts
+++ kemenu-frontend/src/app/components/login/login.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
-
+import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 
@@ -10,7 +10,8 @@ import { TranslateModule } from '@ngx-translate/core';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    TranslateModule
+    TranslateModule,
+    RouterModule
   ],
   declarations: [LoginComponent]
 })
