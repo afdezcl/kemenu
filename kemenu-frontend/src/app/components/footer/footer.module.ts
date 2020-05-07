9da2ff00a94427c9dcbf709e81b1ca696e313cf4
afdezcl <adrifdez_@uma.es>
2020-05-07 22:02:06 +0200

    Texts translated and innecesary svg removed

--- kemenu-frontend/src/app/components/footer/footer.module.ts
+++ kemenu-frontend/src/app/components/footer/footer.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    TranslateModule
   ],
   declarations: [FooterComponent],
   exports: [FooterComponent]
