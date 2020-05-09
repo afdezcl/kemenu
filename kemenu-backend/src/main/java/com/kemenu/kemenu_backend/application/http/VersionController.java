3a47721a0c7a1c47a79a9416503487b44d6701bf
Nicolas Vargas Ortega <nvortega92@gmail.com>
2020-05-09 12:24:46 +0200

    Added version endpoint

---
+++ kemenu-backend/src/main/java/com/kemenu/kemenu_backend/application/http/VersionController.java
@@ -0,0 +1,28 @@
+package com.kemenu.kemenu_backend.application.http;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import lombok.Builder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class VersionController {
+
+    @Value("${app.version}")
+    private String appVersion;
+
+    @GetMapping("/app/version")
+    public VersionResponse getVersion() {
+        return new VersionResponse(appVersion.replace("-SNAPSHOT", ""));
+    }
+
+    @lombok.Value
+    @Builder(toBuilder = true)
+    @JsonDeserialize(builder = VersionResponse.VersionResponseBuilder.class)
+    private static class VersionResponse {
+        String version;
+    }
+}
