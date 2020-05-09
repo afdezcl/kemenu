e41924b20c428fd6e429b7546225205c8c26b2d6
Nicolas Vargas Ortega <nvortega92@gmail.com>
2020-05-09 11:32:32 +0200

    Trying to fix deploy in prod

--- kemenu-backend/src/main/java/com/kemenu/kemenu_backend/infrastructure/mongo/CustomerSpringMongoRepository.java
+++ kemenu-backend/src/main/java/com/kemenu/kemenu_backend/infrastructure/mongo/CustomerSpringMongoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.UUID;
 
-interface CustomerSpringMongoRepository extends MongoRepository<Customer, UUID> {
+public interface CustomerSpringMongoRepository extends MongoRepository<Customer, UUID> {
 }
