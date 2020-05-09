f07b119a23beb2f88c791fa324d37a4bb7d7bcf6
Nicolas Vargas Ortega <nvortega92@gmail.com>
2020-05-09 12:15:40 +0200

    Fixed customer creation

--- kemenu-backend/src/main/java/com/kemenu/kemenu_backend/application/security/SecurityConfig.java
+++ kemenu-backend/src/main/java/com/kemenu/kemenu_backend/application/security/SecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -26,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new Argon2PasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
 
     @Override
