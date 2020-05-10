b47d987f722d0b1dfb91e61fa418c500c45b3e92
Nicolas Vargas Ortega <nvortega92@gmail.com>
2020-05-10 15:53:11 +0200

    Added client recaptcha key to production env, and fixed loginForm invalid

--- kemenu-frontend/src/environments/environment.prod.ts
+++ kemenu-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://kemenu.com/v1'
+  apiBaseUrl: 'https://kemenu.com/v1',
+  recaptchaToken: '6LdTMfUUAAAAANcZwb0O_jEXYMElMEAyYdHmnKeM'
 };
