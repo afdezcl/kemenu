324705747239f20ec82e918b67b0b2e709fb1c9e
Nicolás Vargas Ortega <nvortega92@gmail.com>
2020-05-14 09:10:46 +0200

    Disabled login form and httpBasic auth

--- kemenu-backend/src/main/java/com/kemenu/kemenu_backend/application/security/SecurityConfig.java
+++ kemenu-backend/src/main/java/com/kemenu/kemenu_backend/application/security/SecurityConfig.java
@@ -16,7 +16,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -48,6 +47,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .cors(withDefaults())
                 .csrf().disable()
+                .httpBasic().disable()
+                .formLogin().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
@@ -61,8 +62,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
                 .and()
-                .addFilterBefore(new JWTAuthenticationFilter(mapper, recaptcha, authenticationManager(), jwtService), UsernamePasswordAuthenticationFilter.class)
-                .addFilterAfter(new JWTAuthorizationFilter(authenticationManager(), jwtService), UsernamePasswordAuthenticationFilter.class);
+                .addFilter(new JWTAuthenticationFilter(mapper, recaptcha, authenticationManager(), jwtService))
+                .addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtService));
     }
 
     @Override
