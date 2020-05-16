6d5f6a461a27f1c0b7e6cc0d4d9e6c7c061142ef
Nicolas Vargas Ortega <nvortega92@gmail.com>
2020-05-16 17:10:34 +0200

    Refactored QR controller

--- kemenu-backend/src/main/java/com/kemenu/kemenu_backend/application/qr/QRController.java
+++ kemenu-backend/src/main/java/com/kemenu/kemenu_backend/application/qr/QRController.java
@@ -1,39 +1,29 @@
 package com.kemenu.kemenu_backend.application.qr;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-
 @RestController
 @AllArgsConstructor
 @RequestMapping("/web/v1")
-public class QRController {
+class QRController {
 
     private final QRService qrService;
 
     @GetMapping("/qr/customers/{customerId}/businesses/{businessId}/menus/{menuId}")
-    ResponseEntity<String> readMenu(@PathVariable String customerId,
+    ResponseEntity<byte[]> readMenu(@PathVariable String customerId,
                                     @PathVariable String businessId,
-                                    @PathVariable String menuId,
-                                    HttpServletResponse response) {
+                                    @PathVariable String menuId) {
         byte[] qrCode = qrService.generateQRCode(customerId, businessId, menuId);
-
-        try (InputStream inputStream = new ByteArrayInputStream(qrCode)) {
-            StreamUtils.copy(inputStream, response.getOutputStream());
-            response.setContentType(MediaType.IMAGE_PNG_VALUE);
-        } catch (IOException e) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok().build();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_PNG);
+        return new ResponseEntity<>(qrCode, headers, HttpStatus.OK);
     }
 }
