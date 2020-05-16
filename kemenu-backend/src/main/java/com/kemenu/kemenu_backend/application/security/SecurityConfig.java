840fdb03e0702b966c5d277d3311e91135e6e30d
Nicolas Vargas Ortega <nvortega92@gmail.com>
2020-05-16 15:57:26 +0200

    Ignoring demo route

--- kemenu-backend/src/main/java/com/kemenu/kemenu_backend/application/security/SecurityConfig.java
+++ kemenu-backend/src/main/java/com/kemenu/kemenu_backend/application/security/SecurityConfig.java
@@ -77,6 +77,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/*.js")
                 .antMatchers("/*.css")
                 .antMatchers("/error")
+                .antMatchers("/demo")
                 .antMatchers("/");
     }
 
