528f7ff73428dbcdbc4cd7ac0e2a33d05fa43249
afdezcl <adrifdez_@uma.es>
2020-05-16 14:09:47 +0200

    Const with mock-data to demo

---
+++ kemenu-frontend/src/app/models/demo-mock/demo.mock.ts
@@ -0,0 +1,81 @@
+import { Menu } from "@models/menu/menu.model"
+
+export const Demo: Menu = {
+    customerEmail: 'demo@demo.com',
+    sections: [{
+        name: 'Para picar',
+        dishes: [{
+            name: 'Nachos con queso',
+            description: 'Nachos con queso cheddar, jalapeños y aceitunas negras',
+            price: 7.50,
+            allergens: []
+        }, {
+            name: 'Croquetas',
+            description: 'Croquetas de jamón ibérico',
+            price: 4.50,
+            allergens: []
+        }]
+    }, {
+        name: 'Carnes',
+        dishes: [{
+            name: 'Solomillo a la brasa',
+            description: 'Con mojo picón y sal de escamas',
+            price: 10.20,
+            allergens: []
+        }]
+    }, {
+        name: 'Hamburguesas',
+        dishes: [{
+            name: 'César',
+            description: 'Hamburguesa con queso, tomate y lechuga',
+            price: 7.50,
+            allergens: []
+        }, {
+            name: 'Simple',
+            description: 'Hamburguesa con mayonesa simple',
+            price: 7.80,
+            allergens: []
+        }, {
+            name: 'Big-Burguer',
+            description: 'Hamburguesa doble con beacon',
+            price: 9.50,
+            allergens: []
+        }]
+    }, {
+        name: 'Bebidas',
+        dishes: [{
+            name: 'Coca-Cola',
+            description: '',
+            price: 1.80,
+            allergens: []
+        }, {
+            name: 'Cerveza - Caña',
+            description: '',
+            price: 1.20,
+            allergens: []
+        }, {
+            name: 'Agua',
+            description: '',
+            price: 1.20,
+            allergens: []
+        }]
+    }, {
+        name: 'Postres',
+        dishes: [{
+            name: 'Tarta de queso',
+            description: 'Tarta de queso casera',
+            price: 3.50,
+            allergens: []
+        }, {
+            name: 'Natillas',
+            description: 'Natillas caseras de la casa',
+            price: 4.50,
+            allergens: []
+        }, {
+            name: 'Variado de tartas',
+            description: 'Un variado de tartas caseras de la casa',
+            price: 7,
+            allergens: []
+        }]
+    }]
+}
