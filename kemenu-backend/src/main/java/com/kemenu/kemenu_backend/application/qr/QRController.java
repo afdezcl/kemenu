359609a636d65f039c4673501fa524d27aefb2fa
Nicolas Vargas Ortega <nvortega92@gmail.com>
2020-05-16 17:51:25 +0200

    Returning QR base64 encoded

--- kemenu-backend/src/main/java/com/kemenu/kemenu_backend/application/qr/QRController.java
+++ kemenu-backend/src/main/java/com/kemenu/kemenu_backend/application/qr/QRController.java
@@ -1,14 +1,13 @@
 package com.kemenu.kemenu_backend.application.qr;
 
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Base64;
 
 @RestController
 @AllArgsConstructor
@@ -18,12 +17,11 @@ class QRController {
     private final QRService qrService;
 
     @GetMapping("/qr/customers/{customerId}/businesses/{businessId}/menus/{menuId}")
-    ResponseEntity<byte[]> readMenu(@PathVariable String customerId,
+    ResponseEntity<String> readMenu(@PathVariable String customerId,
                                     @PathVariable String businessId,
                                     @PathVariable String menuId) {
         byte[] qrCode = qrService.generateQRCode(customerId, businessId, menuId);
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.IMAGE_PNG);
-        return new ResponseEntity<>(qrCode, headers, HttpStatus.OK);
+        String base64QRCode = Base64.getEncoder().encodeToString(qrCode);
+        return ResponseEntity.ok(base64QRCode);
     }
 }
