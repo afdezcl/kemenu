93978dd4b43e31607eedfb69f627755b619616d9
Nicolas Vargas Ortega <nvortega92@gmail.com>
2020-05-17 13:10:27 +0200

    Fixed register and menu routes

---
+++ kemenu-backend/src/main/java/com/kemenu/kemenu_backend/application/http/AngularRoutesController.java
@@ -0,0 +1,13 @@
+package com.kemenu.kemenu_backend.application.http;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class AngularRoutesController {
+    @RequestMapping(method = {RequestMethod.OPTIONS, RequestMethod.GET}, path = {"/register/**", "/menu/**"})
+    public String forwardAngularPaths() {
+        return "forward:/index.html";
+    }
+}
