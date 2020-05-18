f148bc85d8de89430e53226e21671af17087c551
Nicolas Vargas Ortega <nvortega92@gmail.com>
2020-05-18 20:25:45 +0200

    Testing email

---
+++ kemenu-backend/src/main/java/com/kemenu/kemenu_backend/application/email/EmailService.java
@@ -0,0 +1,42 @@
+package com.kemenu.kemenu_backend.application.email;
+
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class EmailService {
+
+    @Value("${app.sendgrid}")
+    private String sendgridApiKey;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void sendMail() throws IOException {
+        Email from = new Email("test@example.com");
+        String subject = "Sending with SendGrid is Fun";
+        Email to = new Email("nvortega92@gmail.com");
+        Content content = new Content("text/plain", "and easy to do anywhere, even with Java");
+        Mail mail = new Mail(from, subject, to, content);
+
+        SendGrid sg = new SendGrid(sendgridApiKey);
+        Request request = new Request();
+
+        request.setMethod(Method.POST);
+        request.setEndpoint("mail/send");
+        request.setBody(mail.build());
+        Response response = sg.api(request);
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getBody());
+        System.out.println(response.getHeaders());
+    }
+}
