d8e17f8fc84a24f8c9a5a298fdd3c85b96289893
Nicolas Vargas Ortega <nvortega92@gmail.com>
2020-05-20 19:12:56 +0200

    Added validation to update request

--- kemenu-backend/src/main/java/com/kemenu/kemenu_backend/application/menu/UpdateMenuRequest.java
+++ kemenu-backend/src/main/java/com/kemenu/kemenu_backend/application/menu/UpdateMenuRequest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Builder;
 import lombok.Value;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.List;
@@ -18,5 +19,6 @@ public class UpdateMenuRequest {
     @NotBlank
     @Size(max = 255)
     String businessId;
+    @Valid
     List<MenuSectionData> sections;
 }
