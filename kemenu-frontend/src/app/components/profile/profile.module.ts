cb555918a4d1e2293fcda83bf28ef206387c2eea
afdezcl <adrifdez_@uma.es>
2020-05-26 21:40:04 +0200

    Working on user profile

--- kemenu-frontend/src/app/components/profile/profile.module.ts
+++ kemenu-frontend/src/app/components/profile/profile.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
+import { AlertModule } from 'ngx-bootstrap/alert';
+import { DialogModule } from '@ui-controls/dialogs/dialog.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AlertModule.forRoot(),
+    RouterModule,
+    TranslateModule, 
+    DialogModule
   ],
   declarations: [ProfileComponent]
 })
