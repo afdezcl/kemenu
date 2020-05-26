5a5ff1e3781018b024fc993fcef76d9705b2ac66
afdezcl <adrifdez_@uma.es>
2020-05-26 20:11:24 +0200

    Routing and module to user profile created

--- kemenu-frontend/src/app/app-routing.module.ts
+++ kemenu-frontend/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { HomeComponent } from './components/home/home.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { CustomerComponent } from './components/customer/customer.component';
 import { AuthGuard } from '@ui-controls/guards/auth/auth.guard';
+import { ProfileComponent } from './components/profile/profile.component';
 
 export const routes: Routes = [
 
   { path: '', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'menu', component: MenuComponent, canActivate: [AuthGuard]  },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]  },
   { path: 'demo', component: CustomerComponent },
   { path: 'show', component: CustomerComponent },
   { path: '**', redirectTo: '' }
