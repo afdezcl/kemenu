d863e899750fdc3df65751b38e94155243086941
Nicolas Vargas Ortega <nvortega92@gmail.com>
2020-05-26 20:49:47 +0200

    Added servlet config for max file size and added first code for cloudinary integration

--- kemenu-backend/src/main/java/com/kemenu/kemenu_backend/infrastructure/cloudinary/CloudinaryService.java
+++ kemenu-backend/src/main/java/com/kemenu/kemenu_backend/infrastructure/cloudinary/CloudinaryService.java
@@ -1,11 +1,35 @@
 package com.kemenu.kemenu_backend.infrastructure.cloudinary;
 
-import lombok.AllArgsConstructor;
+import com.cloudinary.Cloudinary;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
 
 @Slf4j
 @Service
-@AllArgsConstructor
 public class CloudinaryService {
+
+    private final Cloudinary cloudinary;
+
+    public CloudinaryService(@Value("${app.cloudinary.key}") String key,
+                             @Value("${app.cloudinary.secret}") String secret,
+                             @Value("${app.cloudinary.cloudname}") String cloudName) {
+        this.cloudinary = new Cloudinary(
+                Map.of(
+                        "api_key", key,
+                        "api_secret", secret,
+                        "cloud_name", cloudName
+                )
+        );
+    }
+
+    @SneakyThrows
+    public boolean upload(MultipartFile file) {
+        Map uploadResult = cloudinary.uploader().upload(file.getBytes(), Map.of());
+        return false;
+    }
 }
