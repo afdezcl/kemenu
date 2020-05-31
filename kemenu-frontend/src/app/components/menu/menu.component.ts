910f78ffb9fa23a4e0af10edfebc504890a7d811
afdezcl <adrifdez_@uma.es>
2020-05-31 14:42:03 +0200

    Fix translation to allergens and hide console log on production mode

--- kemenu-frontend/src/app/components/menu/menu.component.ts
+++ kemenu-frontend/src/app/components/menu/menu.component.ts
@@ -1,16 +1,16 @@
-import {Component, OnInit} from '@angular/core';
-import {Menu} from '@models/menu/menu.model';
-import {BsModalRef, BsModalService} from 'ngx-bootstrap/modal';
-import {CreateSectionComponent} from './create-section/create-section.component';
-import {Section} from '@models/menu/section.model';
-import {CreateDishComponent} from './create-dish/create-dish.component';
-import {Dish} from '@models/menu/dish.model';
-import {ConfirmDialogComponent} from '@ui-controls/dialogs/confirmDialog/confirmDialog.component';
-import {TranslateService} from '@ngx-translate/core';
-import {ShareQrComponent} from './share-qr/share-qr.component';
-import {MenuService} from '@services/menu/menu.service';
-import {AuthenticationService} from '@services/authentication/authentication.service';
-import {Allergen, AllAllergens, AllergenRequestResponse} from '@models/menu/allergen.model';
+import { Component, OnInit } from '@angular/core';
+import { Menu } from '@models/menu/menu.model';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { CreateSectionComponent } from './create-section/create-section.component';
+import { Section } from '@models/menu/section.model';
+import { CreateDishComponent } from './create-dish/create-dish.component';
+import { Dish } from '@models/menu/dish.model';
+import { ConfirmDialogComponent } from '@ui-controls/dialogs/confirmDialog/confirmDialog.component';
+import { TranslateService } from '@ngx-translate/core';
+import { ShareQrComponent } from './share-qr/share-qr.component';
+import { MenuService } from '@services/menu/menu.service';
+import { AuthenticationService } from '@services/authentication/authentication.service';
+import { Allergen, AllAllergens } from '@models/menu/allergen.model';
 
 @Component({
   selector: 'app-menu',
@@ -80,7 +80,7 @@ export class MenuComponent implements OnInit {
       message: this.translate.instant('Delete Section description')
     };
 
-    this.modalReference = this.modalService.show(ConfirmDialogComponent, {initialState});
+    this.modalReference = this.modalService.show(ConfirmDialogComponent, { initialState });
     this.modalReference.content.onClose.subscribe((canDelete: boolean) => {
       if (canDelete) {
         this.menu.sections = this.menu.sections.filter(section => section !== sectionToRemove);
@@ -93,7 +93,7 @@ export class MenuComponent implements OnInit {
     const initialState = {
       name: sectionToEdit.name
     };
-    this.modalReference = this.modalService.show(CreateSectionComponent, {initialState});
+    this.modalReference = this.modalService.show(CreateSectionComponent, { initialState });
     this.modalReference.content.messageEvent.subscribe(data => {
       this.menu.sections[sectionIndex].name = data;
       this.thereIsChange = true;
@@ -119,7 +119,7 @@ export class MenuComponent implements OnInit {
       message: this.translate.instant('Delete Dish description'),
     };
 
-    this.modalReference = this.modalService.show(ConfirmDialogComponent, {initialState});
+    this.modalReference = this.modalService.show(ConfirmDialogComponent, { initialState });
     this.modalReference.content.onClose.subscribe((canDelete: boolean) => {
       if (canDelete) {
         this.menu.sections[sectionIndex].dishes =
@@ -136,10 +136,9 @@ export class MenuComponent implements OnInit {
       price: dishToEdit.price,
       selectedAllergens: dishToEdit.allergens
     };
-    this.modalReference = this.modalService.show(CreateDishComponent, {initialState});
+    this.modalReference = this.modalService.show(CreateDishComponent, { initialState });
     this.modalReference.content.messageEvent.subscribe(data => {
       this.menu.sections[sectionIndex].dishes[dishIndex] = data;
-      console.log(this.menu.sections);
       this.matchAllergens();
       this.thereIsChange = true;
     });
@@ -149,7 +148,7 @@ export class MenuComponent implements OnInit {
     const initialState = {
       shortUrlId: this.menu.shortUrlId
     };
-    this.modalReference = this.modalService.show(ShareQrComponent, {initialState});
+    this.modalReference = this.modalService.show(ShareQrComponent, { initialState });
   }
 
   onSaveMenu() {
@@ -182,18 +181,15 @@ export class MenuComponent implements OnInit {
       menuId: this.menu.id,
       sections: menuSections
     };
-    console.log(this.menu);
     this.menuService.updateMenu(menuToUpdate)
       .subscribe((response: string) => {
         this.menu.id = response;
         this.matchAllergens();
-        console.log(this.menu);
       });
   }
 
   sanitizeAllergensMenuToUpdate() {
     const sections = this.menu.sections;
-    console.log(this.menu);
     sections.map((section: Section) => {
       section.dishes.map((dish: Dish) => {
         dish.allergens.map((allergen: Allergen) => delete allergen.imageName);
@@ -210,7 +206,5 @@ export class MenuComponent implements OnInit {
         });
       });
     });
-
-    console.log(this.menu);
   }
 }
