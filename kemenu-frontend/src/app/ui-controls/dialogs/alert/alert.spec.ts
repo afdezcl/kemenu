b4f1666ed18fa21e53b4f5f2defbdefb52e85da2
Alejandro Garcia <alejandro.garcia@lottoland.com>
2020-05-31 13:37:19 +0200

    KEM-45 Fix alert height + simple test for messages

---
+++ kemenu-frontend/src/app/ui-controls/dialogs/alert/alert.spec.ts
@@ -0,0 +1,42 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { AlertComponent } from './alert.component';
+import { RouterModule } from '@angular/router';
+
+fdescribe('Component: Alert', () => {
+
+  let component: AlertComponent;
+  let fixture: ComponentFixture<AlertComponent>;
+  const SUCCESS_TEXT = 'Welcome to KEMENU';
+  const SUCCESS_TYPE = 'success';
+  const DANGER_TEXT = 'Email or password wrong';
+  const DANGER_TYPE = 'danger';
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterModule.forRoot([])],
+      declarations: [AlertComponent]
+    });
+
+    fixture = TestBed.createComponent(AlertComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('Shout create the component alert', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('Should appear message in success type', () => {
+    component.message = {text: SUCCESS_TEXT, type: SUCCESS_TYPE};
+    fixture.detectChanges();
+    const compiled: HTMLElement = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.message').textContent).toBe(SUCCESS_TEXT);
+  });
+
+  it('Should appear message in danger type', () => {
+    component.message = {text: DANGER_TEXT, type: DANGER_TYPE};
+    fixture.detectChanges();
+    const compiled: HTMLElement = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.message').textContent).toBe(DANGER_TEXT);
+  });
+
+});
