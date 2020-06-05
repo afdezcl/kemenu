d93d4684cdc1de4c1a9f3905956774b3888c1281
Alejandro Garcia <alejandro.garcia.maya@gmail.com>
2020-06-05 09:36:05 +0200

    KEM-51 Abous us page

---
+++ kemenu-frontend/src/app/components/aboutUs/aboutUs.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-about-us',
+  templateUrl: './aboutUs.component.html',
+  styleUrls: ['./aboutUs.component.scss']
+})
+export class AboutUsComponent {}
