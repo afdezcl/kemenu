b9dd063c536c99f3591366670d3d46417fe938fa
Nicolas Vargas Ortega <nvortega92@gmail.com>
2020-06-09 09:23:40 +0200

    Removed email confirmation restriction in login

--- kemenu-backend/src/main/java/com/kemenu/kemenu_backend/application/security/CustomerUserDetailsService.java
+++ kemenu-backend/src/main/java/com/kemenu/kemenu_backend/application/security/CustomerUserDetailsService.java
@@ -1,7 +1,5 @@
 package com.kemenu.kemenu_backend.application.security;
 
-import com.kemenu.kemenu_backend.domain.model.ConfirmedEmail;
-import com.kemenu.kemenu_backend.domain.model.ConfirmedEmailRepository;
 import com.kemenu.kemenu_backend.domain.model.CustomerRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,28 +17,14 @@ import java.util.List;
 public class CustomerUserDetailsService implements UserDetailsService {
 
     private final CustomerRepository customerRepository;
-    private final ConfirmedEmailRepository confirmedEmailRepository;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return customerRepository.findByEmail(email)
-                .map(customer -> {
-                    if (hasConfirmedAccount(email)) {
-                        return new User(customer.getEmail(), customer.getPassword(), List.of(customer));
-                    } else {
-                        log.info("A customer with email {} is trying to login with a not confirmed email", email);
-                        throw new UsernameNotFoundException("Invalid email or password.");
-                    }
-                })
+                .map(customer -> new User(customer.getEmail(), customer.getPassword(), List.of(customer)))
                 .orElseThrow(() -> {
                     log.info("Customer with email {} tried to login with incorrect email or password", email);
                     throw new UsernameNotFoundException("Invalid email or password.");
                 });
-    }
-
-    private boolean hasConfirmedAccount(String email) {
-        return confirmedEmailRepository.findByEmail(email)
-                .map(ConfirmedEmail::isConfirmed)
-                .orElse(false);
     }
 }
