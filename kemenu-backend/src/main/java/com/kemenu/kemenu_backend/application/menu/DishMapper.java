a9b40ee9f7f81b5eee7e6445ba650f211ce9de13
Nicolás Vargas Ortega <nvortega92@gmail.com>
2020-06-20 22:56:29 +0200

    Removed unused DishMapper method

--- kemenu-backend/src/main/java/com/kemenu/kemenu_backend/application/menu/DishMapper.java
+++ kemenu-backend/src/main/java/com/kemenu/kemenu_backend/application/menu/DishMapper.java
@@ -1,8 +1,6 @@
 package com.kemenu.kemenu_backend.application.menu;
 
 import com.kemenu.kemenu_backend.application.allergen.AllergenData;
-import com.kemenu.kemenu_backend.domain.model.Allergen;
-import com.kemenu.kemenu_backend.domain.model.Dish;
 import com.kemenu.kemenu_backend.domain.model.MenuSection;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -15,20 +13,6 @@ import static java.util.stream.Collectors.toList;
 @Component
 @AllArgsConstructor
 public class DishMapper {
-
-    public List<Dish> from(MenuSectionRequest menuSectionRequest) {
-        return menuSectionRequest.getDishes().stream()
-                .map(d -> Dish.builder()
-                        .name(d.getName())
-                        .description(d.getDescription())
-                        .price(d.getPrice())
-                        .allergens(d.getAllergens().stream().map(a -> Allergen.builder().id(a.getId()).name(a.getName()).build()).collect(toList()))
-                        .imageUrl(d.getImageUrl())
-                        .available(isNull(d.getAvailable()) ? true : d.getAvailable()) // TODO: Refactor when frontend use it
-                        .build()
-                )
-                .collect(toList());
-    }
 
     public List<DishResponse> from(MenuSection menuSection) {
         return menuSection.getDishes().stream()
