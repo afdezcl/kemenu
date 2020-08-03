a44e1edf34aeb1ae46d179e4b7a0411f995445c4
Nicolas Vargas Ortega <nvortega92@gmail.com>
2020-08-03 19:22:27 +0200

    KEM-84: Add script to make dumps and upload them to google drive at 5 AM every day

---
+++ scripts/db_backups.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+mongodump --db=k3menudatastore --out=.
+
+DUMP_NAME=$(date '+%Y-%m-%d')
+
+tar -czvf "dump_k3menudatastore_${DUMP_NAME}.tar.gz" ./k3menudatastore
+
+rm -rf ./k3menudatastore
+
+# https://developers.google.com/identity/protocols/oauth2/web-server#obtainingaccesstokens <- Info to obtain access_token
+G_CLIENT_ID='637846255671-8c8q5akupisvqc84fofacvil50oo6452.apps.googleusercontent.com'
+G_CLIENT_SECRET='KtJdDVenaRnt-lFOeWvec5X5'
+G_API_KEY='AIzaSyAW25gI3Bsy0cnQP98Zbe-LfpP9oCVDU6o'
+
+# Copy and paste this in browser for get authorization code
+BROWSER_URL="https://accounts.google.com/o/oauth2/v2/auth?client_id=${G_CLIENT_ID}&access_type=offline&redirect_uri=urn:ietf:wg:oauth:2.0:oob&scope=https://www.googleapis.com/auth/drive.file&response_type=code"
+AUTHORIZATION_CODE="4/2gFGUcAb9pt-QhN-cLZO1nXviFjr924MEQgirrf6LL5lIclv8NGbVa0"
+
+#ACCESS_TOKEN_RESPONSE=$(curl -s --request POST --data "code=${AUTHORIZATION_CODE}&client_id=${G_CLIENT_ID}&client_secret=${G_CLIENT_SECRET}&redirect_uri=urn:ietf:wg:oauth:2.0:oob&grant_type=authorization_code" https://oauth2.googleapis.com/token | /usr/bin/jq --raw-output '.')
+REFRESH_TOKEN="1//03TpOC7W3oj4GCgYIARAAGAMSNwF-L9IrShkcGJYGDmIhHQvqTUha_DwNcuSIUKxQtAb4ZO_PpsIWpiiCfUsghGxp0rd0_OawN64"
+
+# if [[ -z "${ACCESS_TOKEN}" || "${ACCESS_TOKEN}" == "null" ]]; then
+ACCESS_TOKEN_RESPONSE=$(curl -s --request POST --data "client_id=${G_CLIENT_ID}&client_secret=${G_CLIENT_SECRET}&refresh_token=${REFRESH_TOKEN}&grant_type=refresh_token" https://oauth2.googleapis.com/token | /usr/bin/jq --raw-output '.')
+ACCESS_TOKEN=$(echo $ACCESS_TOKEN_RESPONSE | jq .access_token)
+# fi
+
+curl -s -X POST -L \
+    -H "Authorization: Bearer ${ACCESS_TOKEN}" \
+    -F "metadata={name : '${}'};type=application/json;charset=UTF-8" \
+    -F "file=@${};type=application/gzip" \
+    "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart"
