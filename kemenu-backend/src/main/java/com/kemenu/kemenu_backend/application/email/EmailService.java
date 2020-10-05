749627f8be6324f826016edb30d3ede6497229ab
Nicolas Vargas Ortega <nvortega92@gmail.com>
2020-10-05 20:15:04 +0200

    Removed sneaky throws

--- kemenu-backend/src/main/java/com/kemenu/kemenu_backend/application/email/EmailService.java
+++ kemenu-backend/src/main/java/com/kemenu/kemenu_backend/application/email/EmailService.java
@@ -8,10 +8,11 @@ import com.sendgrid.SendGrid;
 import com.sendgrid.helpers.mail.Mail;
 import com.sendgrid.helpers.mail.objects.Content;
 import com.sendgrid.helpers.mail.objects.Email;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Slf4j
 @Service
@@ -20,24 +21,27 @@ public class EmailService {
     @Value("${app.sendgrid}")
     private String sendgridApiKey;
 
-    @SneakyThrows
     public void sendMail(SendEmailEvent event, String type, String contentWithUrl) {
-        Email from = new Email(event.getFrom());
-        String subject = event.getSubject();
-        Email to = new Email(event.getTo());
-        Content content = new Content(type, contentWithUrl);
-        Mail mail = new Mail(from, subject, to, content);
+        try {
+            Email from = new Email(event.getFrom());
+            String subject = event.getSubject();
+            Email to = new Email(event.getTo());
+            Content content = new Content(type, contentWithUrl);
+            Mail mail = new Mail(from, subject, to, content);
 
-        SendGrid sg = new SendGrid(sendgridApiKey);
-        Request request = new Request();
+            SendGrid sg = new SendGrid(sendgridApiKey);
+            Request request = new Request();
 
-        request.setMethod(Method.POST);
-        request.setEndpoint("mail/send");
-        request.setBody(mail.build());
-        Response response = sg.api(request);
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
 
-        if (response.getStatusCode() > 299) {
-            log.warn("Email sent failed with status code {}, body {} and headers {}", response.getStatusCode(), response.getBody(), response.getHeaders());
+            if (response.getStatusCode() > 299) {
+                log.warn("Email sent failed with status code {}, body {} and headers {}", response.getStatusCode(), response.getBody(), response.getHeaders());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
