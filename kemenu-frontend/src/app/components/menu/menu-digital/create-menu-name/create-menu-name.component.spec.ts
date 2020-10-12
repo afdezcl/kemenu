424764e476035c1e823a081fea7c69d84c04bcf1
Nicolas Vargas Ortega <nvortega92@gmail.com>
2020-10-12 20:20:33 +0200

    KEM-128: Fix frontend test

--- kemenu-frontend/src/app/components/menu/menu-digital/create-menu-name/create-menu-name.component.spec.ts
+++ kemenu-frontend/src/app/components/menu/menu-digital/create-menu-name/create-menu-name.component.spec.ts
@@ -1,28 +1,12 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import {async} from '@angular/core/testing';
 
-import { CreateMenuNameComponent } from './create-menu-name.component';
+import {CreateMenuNameComponent} from './create-menu-name.component';
 
 describe('CreateMenuNameComponent', () => {
-  let component: CreateMenuNameComponent;
-  let fixture: ComponentFixture<CreateMenuNameComponent>;
-
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CreateMenuNameComponent ]
-    })
-    .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CreateMenuNameComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
   });
 });
