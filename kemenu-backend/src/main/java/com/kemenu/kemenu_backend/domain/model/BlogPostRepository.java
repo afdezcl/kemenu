fed670643c04c0a6134e94c12658cb79e46f5110
Nicolas Vargas Ortega <nvortega92@gmail.com>
2020-10-22 21:08:51 +0200

    KEM-105: working in the blog post getters

--- kemenu-backend/src/main/java/com/kemenu/kemenu_backend/domain/model/BlogPostRepository.java
+++ kemenu-backend/src/main/java/com/kemenu/kemenu_backend/domain/model/BlogPostRepository.java
@@ -3,5 +3,5 @@ package com.kemenu.kemenu_backend.domain.model;
 import java.util.Optional;
 
 public interface BlogPostRepository {
-    Optional<BlogPost> findById(String id);
+    Optional<BlogPost> findByReadableId(String readableId);
 }
